fdc8a2961b20046aa28302260c13589d3bb25f3e
Elena Palesis <elenapalesis@gmail.com>
2018-03-09 19:20:08 -0800

    Add failing specs

--- spec/features/admin/poem_listing_spec.rb
+++ spec/features/admin/poem_listing_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 feature 'Admin poem listing' do
 
   scenario 'admin can view all poems' do
-    poem1 = create(:poem)
-    poem2 = create(:poem)
-    puts poem1.inspect
-    puts poem2.inspect
+    poem1 = create(:poem, english_title: 'English Title 1', greek_title: 'Greek Title 1')
+    poem2 = create(:poem, english_title: 'English Title 2', greek_title: 'Greek Title 2')
+
+    visit '/admin/poems'
+
+    expect(page).to have_selector('li', text: 'Greek Title 1 (English Title 1)')
+    expect(page).to have_selector('li', text: 'Greek Title 2 (English Title 2)')
   end
 
 end
