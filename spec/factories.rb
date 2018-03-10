90b6f06f49c405d1debcae7f58f795fb0e639208
Elena Palesis <elenapalesis@gmail.com>
2018-03-09 20:23:50 -0800

    Appease robocop

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
-
   factory :poem do
     english_title 'Example title'
     greek_title 'Ενα τιτλος'
     english_body 'Lorem ipsum dolores park amen'
     greek_body 'Λορεμ ιπσουμ δολορεσ παρκο αμιν'
   end
-
 end
--- spec/features/admin/poem_listing_spec.rb
+++ spec/features/admin/poem_listing_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 feature 'Admin poem listing' do
-
   scenario 'admin can view all poems' do
     poem1 = create(:poem, english_title: 'English Title 1', greek_title: 'Greek Title 1')
     poem2 = create(:poem, english_title: 'English Title 2', greek_title: 'Greek Title 2')
@@ -11,5 +10,4 @@ feature 'Admin poem listing' do
     expect(page).to have_selector('li', text: 'Greek Title 1 (English Title 1)')
     expect(page).to have_selector('li', text: 'Greek Title 2 (English Title 2)')
   end
-
 end
