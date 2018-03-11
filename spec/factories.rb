7ba94266ce25b912704e32219cfaee5a3d00cc60
Elena Palesis <elenapalesis@gmail.com>
2018-03-10 20:05:47 -0800

    Alphabetize attributes of poem factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :poem do
-    english_title 'Example title'
-    greek_title 'Ενα τιτλος'
     english_body 'Lorem ipsum dolores park amen'
+    english_title 'Example title'
     greek_body 'Λορεμ ιπσουμ δολορεσ παρκο αμιν'
+    greek_title 'Ενα τιτλος'
   end
 end
