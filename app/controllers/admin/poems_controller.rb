5921e929318d35b60cbe88c2728a3bbf64b871a8
Elena Palesis <elenapalesis@gmail.com>
2018-03-13 00:54:53 -0700

    Add form to view and edit controller to support form

--- app/controllers/admin/poems_controller.rb
+++ app/controllers/admin/poems_controller.rb
@@ -1,5 +1,25 @@
 class Admin::PoemsController < ApplicationController
   def index
+    # instantiate poem for use in new poem form (#create action)
+    @poem = Poem.new
+
     @poems = Poem.order(:id)
+  end
+
+  def create
+    @poem = Poem.new(poem_params)
+    @poem.save
+
+    # after submitting new poem, redirect back to poems index
+    redirect_to(admin_poems_path)
+  end
+
+  private
+
+  # Whitelist the poem parameters used to define a poem. Otherwise, an attempted
+  # assignemnt will throw ActiveModel::ForbiddenAttributesError.
+  def poem_params
+    params.require(:poem).permit(:greek_title, :english_title, :greek_body, :english_body,
+      :published)
   end
 end
