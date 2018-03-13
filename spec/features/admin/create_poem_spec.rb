1bc32732c0c3094f256e8e31b9c99ef8fdd7765e
Elena Palesis <elenapalesis@gmail.com>
2018-03-13 00:47:23 -0700

    Update feature spec to reflect form_for id

--- spec/features/admin/create_poem_spec.rb
+++ spec/features/admin/create_poem_spec.rb
@@ -4,12 +4,12 @@ feature 'Admin creates a new poem' do
   scenario 'they see the poem on the index' do
     visit '/admin/poems'
 
-    fill_in 'greek_title', with: 'Titlos Alpha'
-    fill_in 'english_title', with: 'Title A'
-    fill_in 'greek_body', with: 'A Greek body is a wonderful thing.'
-    fill_in 'english_body', with: 'An English body is less sought after.'
-    page.check('publish')
-    click_button 'create_poem'
+    fill_in 'poem_greek_title', with: 'Titlos Alpha'
+    fill_in 'poem_english_title', with: 'Title A'
+    fill_in 'poem_greek_body', with: 'A Greek body is a wonderful thing.'
+    fill_in 'poem_english_body', with: 'An English body is less sought after.'
+    page.check('poem_published')
+    click_button 'Create Poem'
 
     expect(page).to have_selector('li', text: 'Titlos Alpha (Title A)')
   end
